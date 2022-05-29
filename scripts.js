0e3b54dc23a6453cc0a299ef5b297ac8b79303bc
Jorge Linero <jorge.linero.r@gmail.com>
2022-05-29 13:09:55 -0300

    personalização misturado com o projeto tinder-clone para colocar o relogio responsivo e troca do gradiente de fondo pelo canvas

--- scripts.js
+++ scripts.js
@@ -1,6 +1,46 @@
 const hoursHand = document.querySelector(".hand.hours");
 const minutesHand = document.querySelector(".hand.minutes");
 const secondsHand = document.querySelector(".hand.seconds");
+const hoursDigital = document.querySelector(".digitalHour");
+const minutesDigital = document.querySelector(".digitalMinute");
+const secondsDigital = document.querySelector(".digitalSecond");
+var dsp = document.getElementById("canvas");
+var ctx = dsp.getContext("2d");
+var can = document.querySelector("canvas");
+can.style.position = "absolute";
+can.style.top = "0px";
+can.style.left = "0px";
+
+dsp.height = window.innerWidth * 1.5;
+dsp.width = window.innerWidth;
+
+var numbersRain =
+  "1001010101110101010101010010101000101011101111010101010110101010101010101110000101";
+numbersRain = numbersRain.split("");
+
+var font_size = 20;
+var columns = dsp.width / font_size;
+var drops = [];
+for (var x = 0; x < columns; x++) drops[x] = 1;
+
+const draw = () => {
+  ctx.fillStyle = "rgba(255, 255, 255, 0.05)";
+  ctx.fillRect(0, 0, dsp.width, dsp.height);
+
+  ctx.fillStyle = "#666"; //green text
+  ctx.font = font_size + "px arial";
+
+  for (var i = 0; i < drops.length; i++) {
+    var text = numbersRain[Math.floor(Math.random() * numbersRain.length)];
+
+    ctx.fillText(text, i * font_size, drops[i] * font_size);
+
+    if (drops[i] * font_size > dsp.height && Math.random() > 0.975)
+      drops[i] = 0;
+
+    drops[i]++;
+  }
+};
 
 const setRotation = (element, rotationPercentage) => {
   element.style.setProperty("--rotation", rotationPercentage * 360);
@@ -16,8 +56,21 @@ const setClock = () => {
   setRotation(secondsHand, secondsPercentage);
   setRotation(minutesHand, minutesPercentage);
   setRotation(hoursHand, hoursPercentage);
+
+  secondsDigital.textContent = zeroFilling(currentDate.getSeconds(), 2);
+  minutesDigital.textContent = zeroFilling(currentDate.getMinutes(), 2);
+  hoursDigital.textContent = zeroFilling(currentDate.getHours(), 2);
+};
+
+const zeroFilling = (number, quntity) => {
+  var zero = "";
+  for (var i = 0; i < quntity; i++) {
+    zero += "0";
+  }
+  return (zero + number).slice(-quntity);
 };
 
 setClock();
 
 setInterval(setClock, 1000);
+setInterval(draw, 33);
